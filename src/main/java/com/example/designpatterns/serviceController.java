6d18f4f66603327ae8a06dabb44bd808f4c37798
worthyd <ozgameren@gmail.com>
2021-10-20 03:51:11 +0300

    changes

--- src/main/java/com/example/designpatterns/serviceController.java
+++ src/main/java/com/example/designpatterns/serviceController.java
@@ -1,7 +1,14 @@
 package com.example.designpatterns;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping
+
+@RestController
 public class serviceController {
+
+    @RequestMapping("/")
+    public String hello() {
+        return  "selamlar";
+    }
 }
